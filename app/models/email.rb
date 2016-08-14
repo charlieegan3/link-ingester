67327320d6499bd15268be18b6df8e1f98d6d5b8
Charlie Egan <git@charlieegan3.com>
2016-08-14 15:20:20 +0100

    Extract email links from html
    This uses either the html anchors or a regex to extract links in the
    email body. These will be used to aggregate the link feed.
    
    Text is also extracted to filter links from expansion.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,11 @@
 class Email < ApplicationRecord
   validates :html, presence: true
+
+  def links
+    if html.match(/\/>|<a|<p|\w+="/)
+      Nokogiri::HTML(html).css("a").map { |a| { text: a.text, url: a["href"] } }
+    else
+      html.scan(/https?\S+/).map { |url| { text: nil, url: url } }
+    end
+  end
 end
--- test/models/email_test.rb
+++ test/models/email_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class EmailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "links are extracted from HTML" do
+    email = emails(:simple)
+    assert email.links == [{ text: "Charlie", url: "http://charlie.com" }]
+  end
+
+  test "links are extracted from plain body emails" do
+    email = emails(:plain)
+    assert email.links == [{ text: nil, url: "http://charlie.com" }]
+  end
 end
