eddf7af7ced53d1a8725de1574ea79f0e8b4ab3e
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-08-21 18:11:46 +0100

    Handle invalid URIs by returning the raw URL
    Some urls can't be parsed by URI.parse

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,8 +2,12 @@ class Link < ApplicationRecord
   has_many :email_links, dependent: :destroy
 
   def pretty_url
-    uri = URI.parse(self.url)
-    "#{uri.host}#{uri.path}".gsub(/^www\.|\/+$/, "")
+    begin
+      uri = URI.parse(self.url)
+      "#{uri.host}#{uri.path}".gsub(/^www\.|\/+$/, "")
+    rescue
+      self.url
+    end
   end
 
   def self.find_or_create_by_url(url)
